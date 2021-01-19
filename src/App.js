9f542f4d2744ccadfa833870088512b12e2c6f40
rand alsallaq <randalsallaq97@gmail.com>
2021-01-19 22:44:19 +0200

    not done yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header(props){
+  return(
+    <div>
+  <h1>Cill Musical School</h1>
+  <h3>Number of Courses Available in Our Institute: {props.instrumentsList.length}</h3>
     </div>
-  );
+  
+    )
+
 }
 
+
+// function InstList(props){
+//   return(
+    
+//     <h4>Instrumints Available: {props.instrumentsList.map(inst => )}
+//     </h4>
+
+//   )
+
+// }
+
+
+// function Footer(props){
+
+// }
+
+class App extends React.Component{
+constructor(){
+  super();
+  this.state={
+    thingList : [
+      {
+        id: 1,
+        name: 'Guitar'
+      },
+      {
+        id: 2,
+        name: 'Piano'
+      },
+      {
+        id: 3,
+        name: 'Violin'
+      }
+    ]
+  }
+}
+
+render(){
+  return(
+   <>
+    <Header instrumentsList = {this.state.thingList} />
+    
+    </>
+   
+  );
+}
+}
 export default App;
